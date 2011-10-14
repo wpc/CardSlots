301b4dc1ad3279c4cc806e449ca177dc9bd8bba4
wpc <alex.hal9000@gmail.com>
2011-10-14 10:22:33 -0700

    different transparency when app is resign active

--- AppDelegate.rb
+++ AppDelegate.rb
@@ -12,7 +12,15 @@ class AppDelegate
     
     def applicationDidFinishLaunching(a_notification)
         window.opaque = false
-		window.alphaValue = 0.65
+		window.alphaValue = 0.8
         window.level = NSFloatingWindowLevel
+    end
+    
+    def applicationDidResignActive(a_notification)
+        window.alphaValue = 0.55
+    end
+    
+    def applicationWillBecomeActive(a_notification)
+        window.alphaValue = 0.8
     end
 end
